fddd6c759d6c39dd0171d63bf9e95ee201d13ca7
Xiang Li <xlees58@gmail.com>
2015-12-14 17:58:20 +0800

    利用spark统计票子净流入金额。

--- src/main/scala/com/xlees/sample/Stat.scala
+++ src/main/scala/com/xlees/sample/Stat.scala
@@ -22,9 +22,32 @@ object Stat {
         val data = sc.textFile(fpath, 100)
         data.cache()
         
-        val cnt = data.count()
+        data.map(x => {
+        	val line = x.split(",")
+        	val key = (line(0),line(1))
+        	val value = (line(3), line(5).toInt,line(6).toInt)
+        	
+        	(key, value)
+        }).aggregateByKey((0,0), 1)((x:(Int,Int),value:(String,Int,Int))=> {
+        	if (value._1 == "1")
+        		(x._1, x._2+value._3)
+        	else if (value._1 == "2")
+        		(x._1+value._3, x._2)
+        	else
+        		(x._1, x._2)
+        }, (x:(Int,Int), y:(Int,Int)) => {
+        	(x._1+y._1, x._2+y._2)
+        }).map(x => {
+        	val net = x._2._1-x._2._2
+        	val value = (x._2._1,x._2._2,net)
+        	(x._1, value)
+        })
+          .sortBy(x=>x._2._3, false)
+          .saveAsTextFile("result")
+        
+//        val cnt = data.count()
 //        
-        println("num of data: "+cnt)
+//        println("num of data: "+cnt)
         
         Thread.sleep(5)
         
